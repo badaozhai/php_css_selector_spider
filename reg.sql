227adba978caadf3234e464184071045572a6993
bdz_work <53670899@qq.com>
2015-08-13 14:44:52 +0800

    添加sql注释

--- reg.sql
+++ reg.sql
@@ -1,39 +1,43 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50540
+Source Server         : 本地
+Source Server Version : 50621
 Source Host           : localhost:3306
 Source Database       : spider
 
 Target Server Type    : MYSQL
-Target Server Version : 50540
+Target Server Version : 50621
 File Encoding         : 65001
 
-Date: 2015-08-12 13:22:20
+Date: 2015-08-13 14:40:05
 */
 
 SET FOREIGN_KEY_CHECKS=0;
-
 -- ----------------------------
--- Table structure for reg
+-- Table structure for `reg`
 -- ----------------------------
 DROP TABLE IF EXISTS `reg`;
 CREATE TABLE `reg` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL COMMENT '名称',
   `info` varchar(255) DEFAULT NULL,
-  `image_demo` varchar(255) DEFAULT NULL,
-  `unit_reg` varchar(255) DEFAULT NULL,
-  `name_reg` varchar(255) DEFAULT NULL,
-  `phone_reg` varchar(255) DEFAULT NULL,
+  `image_demo` varchar(255) DEFAULT NULL COMMENT '按理截图',
+  `unit_reg` varchar(255) DEFAULT NULL COMMENT '单元格正则',
+  `name_reg` varchar(255) DEFAULT NULL COMMENT '名称正则',
+  `phone_reg` varchar(255) DEFAULT NULL COMMENT '电话正则',
   `page_key` varchar(255) DEFAULT NULL COMMENT '分页字母',
-  `max_page` int(11) DEFAULT '10',
+  `max_page` int(11) DEFAULT '10' COMMENT '所需要抓取的最大页数',
+  `is_in_path` tinyint(4) DEFAULT '1' COMMENT '分页词是否在路径中，默认是1在路径中，否则就是?p=2这种形式',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Table structure for user
+-- Records of reg
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `user`
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
@@ -45,3 +49,7 @@ CREATE TABLE `user` (
   `create_time` int(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=222 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
